ca6e1b8083021c93902bb32c46df4cadca955af5
Eduardo Marossi <7621220+eduardomarossi@users.noreply.github.com>
2021-06-24 18:13:58 -0300

    Update settings.py

--- hc/settings.py
+++ hc/settings.py
@@ -148,7 +148,7 @@ PING_ENDPOINT = os.getenv("PING_ENDPOINT", SITE_ROOT + "/ping/")
 PING_EMAIL_DOMAIN = os.getenv("PING_EMAIL_DOMAIN", "localhost")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "static-collected")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
